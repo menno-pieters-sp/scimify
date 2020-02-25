8a7a96352d801a3af9adf67d3c7ca45caa2cee84
Menno Pieters <menno.pieters@sailpoint.com>
2020-02-25 10:51:47 +0100

    Auto-format

--- resources/config-template.php
+++ resources/config-template.php
@@ -8,11 +8,11 @@
  * recommend testing scripts in a preview environment if possible.
  */
 
-/* MySQL configuration */ 
+/* MySQL configuration */
 define("SCIMIFY_DB_USERNAME", "root");
 define("SCIMIFY_DB_PASSWORD", "");
 define("SCIMIFY_DB_SERVER", "localhost");
-define("SCIMIFY_DB_NAME","scimify");
+define("SCIMIFY_DB_NAME", "scimify");
 
 /* Other configuration */
 error_reporting(0);
